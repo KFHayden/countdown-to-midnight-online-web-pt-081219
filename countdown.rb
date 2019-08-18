18faa8df66f9e509c33b597e53f38b94f6da195b
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-18 01:57:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,8 @@
-def loop
-  
-  def countdown(number)
-    number = 10
-    while number > 0
-     puts "#{number} SECOND(S)!"
-      number -= 1
-    end
-    return "HAPPY NEW YEAR!"
+def countdown(number)
+  number = 10
+  while number > 0
+   puts "#{number} SECOND(S)!"
+    number -= 1
   end
+  return "HAPPY NEW YEAR!"
 end
